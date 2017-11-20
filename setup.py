f62a65c6998648fd491256db81ca3ce5137ceae8
Rymdlego <jakob@rymdlego.se>
2017-11-20 20:16:02 +0100

    Added project to Github

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name = "cidrping",
+	version = "0.1",
+	author = "Jakob Svanholm",
+	author_email = "jakob@rymdlego.se",
+	url = "https://github.com/rymdlego/pingvin",
+	description = "A simple script for pinging hosts by CIDR-notation.",
+	keywords = "network ping cidr",
+	scripts=['cidrping'],
+	install_requires=['ipcalc']
+)
